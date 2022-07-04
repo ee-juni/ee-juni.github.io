3ec035fa730b456ee5dfd4e7b66aa4b7097d6819
Juni Lee <wnsdml0120@korea.ac.kr>
2022-07-04 11:52:50 +0900

    make JS files

---
+++ week2/day1/eron/js_ex/js_02.js
@@ -0,0 +1,89 @@
+/*
+// Dark mode
+document.querySelector('body').style.backgroundColor = 'black';
+document.querySelector('body').style.color = 'white';
+document.querySelector('p').style.borderColor = 'white';
+
+
+// User interaction
+alert('Mayday!'); // 단순 메세지 전달 
+prompt('키미노 나마에와'); // python input
+confirm('ㄹㅇ?'); // true or false 반환
+
+// Print
+document.write('웹 화면에 띄움');
+console.log("콘솔에 띄움");
+
+// 응용
+let name = prompt('이름이 모에요?')
+document.write(name);
+
+// 변수
+let variable; // 변수 선언 후
+variable = 10; // 변수 값 지정
+console.log(variable); // 콘솔 출력
+
+// 변수 선언
+var variable = 10;
+variable = 10; //재할당 가능
+var variable = 20; //재선언 가능
+
+let variable2 = 10;
+variable2 = 20; //재할당 가능, 재선언 불가능
+
+const variable3 = 10; //상수 선언. 재할당 및 선언 불가능
+
+// Number
+let num1 = 3;
+let num2 = 7;
+document.write(num1 + num2);
+document.write("<br/>");
+document.write(num1 - num2);
+document.write("<br/>");
+document.write(num1 * num2);
+document.write("<br/>");
+document.write(num1 / num2);
+document.write("<br/>");
+document.write(num1 % num2);
+document.write("<br/>");
+document.write(Math.PI);
+document.write("<br/>");
+document.write(Math.ceil(3.123));
+document.write("<br/>");
+document.write(Math.power(4,3));
+document.write("<br/>");
+
+// String
+document.write("Hello Elice~")
+document.write("<br/>");
+document.write("Hello Elice~ <br/>")
+document.write("<br/>");
+document.write("Hello\
+Elice~")
+document.write("<br/>");
+document.write("Hello
+Elice~")
+document.write("<br/>");
+document.write("Hello Elice~"[6])
+document.write("<br/>");
+document.write("Hello Elice~".length)
+document.write("<br/>");
+document.write("Hello Elice~".search("E"))
+document.write("<br/>");
+
+// Function
+
+function printText(text){
+    document.write(text); return "OK";
+};
+
+let printText2 = function(text){
+    document.write(text);
+    return "OK2";
+};
+
+printText('안녕?');
+printText2('인녕하세요?');
+
+*/
+
