93ee281670f2e4d6cbff50a921aa09331ac3ff19
Juni Lee <wnsdml0120@korea.ac.kr>
2022-07-06 14:15:03 +0900

    update 2-3 materials

---
+++ week2/day3/eron/ex_01/js_01-1.js
@@ -0,0 +1,11 @@
+function DM(){
+document.querySelector('body').style.backgroundColor = 'black';
+document.querySelector('body').style.color = 'white';
+document.querySelector('p').style.borderColor = 'white';
+}
+
+function LM(){
+document.querySelector('body').style.backgroundColor = 'white';
+document.querySelector('body').style.color = 'black';
+document.querySelector('p').style.borderColor = 'black';
+}
