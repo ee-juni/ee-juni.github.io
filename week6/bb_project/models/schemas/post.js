26f8e6446010a243dff438e70375ad89515193d9
Juni Lee <wnsdml0120@korea.ac.kr>
2022-08-02 09:42:51 +0900

    start week6-2

---
+++ week6/bb_project/models/schemas/post.js
@@ -0,0 +1,11 @@
+const mongoose, { Schema } = require("mongoose")
+const shortId = require("./types/short-id")
+
+module.exports = new Schema({
+    shortId,
+    title: String,
+    content: String,
+    author: String,
+},{
+    timestamps: true
+})
